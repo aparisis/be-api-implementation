558b66d01b0dfe6aeaec5a55060744eaf4362599
Konstantina Zafeiriadou <kzafeiriadou@jokersattractions.com>
2021-03-31 16:32:42 +0300

    FEAT - add functional tests to check if routes declaration can perform successful requests

--- tests/Controller/ControllerTest.php
+++ tests/Controller/ControllerTest.php
@@ -8,24 +8,28 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class ControllerTest extends WebTestCase
 {
     /**
-     * @dataProvider provideGetUrls
+     * @dataProvider provideUrls
+     * @param string $method
      * @param string $url
      * @param int|null $parameter
+     * @param string|null $content
      */
-    public function testGetRequestIsSuccessful(string $url, ?int $parameter){
+    public function testRequestIsSuccessful(string $method, string $url, ?int $parameter, ?string $content){
         $client = self::createClient();
-        $client->request('GET', $url.$parameter ?? '');
-        
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $client->request($method, $url . $parameter ?? '', [], [], [], $content);
+
         $this->assertInstanceOf(JsonResponse::class, $client->getResponse());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
-    public function provideGetUrls(): array
+    public function provideUrls(): array
     {
         return [
-            ['/user/top', null],
-            ['/ticket/', 1]
+            ['GET', '/user/top', null, null],
+            ['GET', '/ticket/', 1, null],
+            ['POST', '/ticket', null, '{"bets": [{"amount": 1.5,"type": "straight"}, {"amount": 2,"type": "split"}]}'],
+            ['PATCH', '/user/', 1, '{"user": {"username":"testuser", "password":"xGfdsek20g"}}']
         ];
     }
 }
