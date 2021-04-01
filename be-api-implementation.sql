9193ba4fee5d3804b45c854e1ed7bf96118529ee
Konstantina Zafeiriadou <kzafeiriadou@jokersattractions.com>
2021-04-01 14:53:50 +0300

    FEAT - add sample data to the database

--- be-api-implementation.sql
+++ be-api-implementation.sql
@@ -1,97 +1,198 @@
-/*
-SQLyog Community v13.1.7 (64 bit)
-MySQL - 5.7.21-log : Database - be-api-implementation
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 01, 2021 at 11:48 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.4.12
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`be-api-implementation` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `be-api-implementation`;
+--
+-- Database: `be-api-implementation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bet`
+--
+
+DROP TABLE IF EXISTS `bet`;
+CREATE TABLE IF NOT EXISTS `bet` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ticket_id` int(10) UNSIGNED NOT NULL,
+  `amount` decimal(14,2) NOT NULL,
+  `bet_type_id` smallint(5) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ticket_id` (`ticket_id`),
+  KEY `bet_type_id` (`bet_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bet`
+--
+
+INSERT INTO `bet` (`id`, `ticket_id`, `amount`, `bet_type_id`) VALUES
+(1, 1, '16500.00', 2),
+(2, 1, '3500.00', 9),
+(3, 1, '46500.00', 10),
+(4, 2, '24500.00', 5),
+(5, 2, '14000.00', 9),
+(6, 3, '28000.00', 6),
+(7, 3, '18500.00', 1),
+(8, 3, '150000.00', 14),
+(9, 4, '500.00', 3),
+(10, 4, '500.00', 1),
+(11, 4, '6500.00', 10),
+(12, 5, '46500.00', 11),
+(13, 5, '23500.00', 7),
+(14, 5, '3500.00', 1),
+(15, 6, '100000.00', 7),
+(16, 7, '500.00', 1),
+(17, 8, '5000.00', 4),
+(18, 9, '15000.00', 10),
+(19, 10, '2000.00', 1),
+(21, 10, '1500.00', 12),
+(22, 11, '500.00', 1),
+(23, 12, '16500.00', 7),
+(24, 13, '1000.00', 2),
+(25, 14, '4000.00', 8),
+(26, 14, '500.00', 1),
+(27, 15, '3500.00', 7),
+(28, 16, '5500.00', 14);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bet_type`
+--
 
 DROP TABLE IF EXISTS `bet_type`;
-
-CREATE TABLE `bet_type` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `bet_type` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `multi` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Data for the table `bet_type` */
+--
+-- Dumping data for table `bet_type`
+--
 
-insert  into `bet_type`(`id`,`name`,`multi`) values (1,'straight',36);
-insert  into `bet_type`(`id`,`name`,`multi`) values (2,'split',18);
-insert  into `bet_type`(`id`,`name`,`multi`) values (3,'trio',12);
-insert  into `bet_type`(`id`,`name`,`multi`) values (4,'street',12);
-insert  into `bet_type`(`id`,`name`,`multi`) values (5,'corner',9);
-insert  into `bet_type`(`id`,`name`,`multi`) values (6,'sixLine',6);
-insert  into `bet_type`(`id`,`name`,`multi`) values (7,'dozen',3);
-insert  into `bet_type`(`id`,`name`,`multi`) values (8,'odd',2);
-insert  into `bet_type`(`id`,`name`,`multi`) values (9,'even',2);
-insert  into `bet_type`(`id`,`name`,`multi`) values (10,'column',3);
-insert  into `bet_type`(`id`,`name`,`multi`) values (11,'red',2);
-insert  into `bet_type`(`id`,`name`,`multi`) values (12,'black',2);
-insert  into `bet_type`(`id`,`name`,`multi`) values (13,'low',2);
-insert  into `bet_type`(`id`,`name`,`multi`) values (14,'high',2);
+INSERT INTO `bet_type` (`id`, `name`, `multi`) VALUES
+(1, 'straight', 36),
+(2, 'split', 18),
+(3, 'trio', 12),
+(4, 'street', 12),
+(5, 'corner', 9),
+(6, 'sixLine', 6),
+(7, 'dozen', 3),
+(8, 'odd', 2),
+(9, 'even', 2),
+(10, 'column', 3),
+(11, 'red', 2),
+(12, 'black', 2),
+(13, 'low', 2),
+(14, 'high', 2);
 
+-- --------------------------------------------------------
 
-/*Table structure for table `user` */
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-/*Data for the table `user` */
-
-/*Table structure for table `ticket` */
+--
+-- Table structure for table `ticket`
+--
 
 DROP TABLE IF EXISTS `ticket`;
-
-CREATE TABLE `ticket` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `winning_amount` decimal(14,2) unsigned NOT NULL DEFAULT '0.00',
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `winning_amount` decimal(14,2) UNSIGNED NOT NULL DEFAULT '0.00',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Data for the table `ticket` */
+--
+-- Dumping data for table `ticket`
+--
 
-/*Table structure for table `bet` */
+INSERT INTO `ticket` (`id`, `user_id`, `winning_amount`, `created`) VALUES
+(1, 1, '25000.00', '2021-03-31 19:17:10'),
+(2, 1, '47500.00', '2021-03-31 19:21:23'),
+(3, 2, '0.00', '2021-03-31 19:29:21'),
+(4, 2, '180000.00', '2021-03-31 19:29:38'),
+(5, 2, '125000.00', '2021-03-31 19:30:02'),
+(6, 3, '0.00', '2021-04-01 13:22:56'),
+(7, 3, '4500.00', '2021-04-01 13:22:56'),
+(8, 4, '1000.00', '2021-04-01 13:26:43'),
+(9, 5, '50000.00', '2021-04-01 13:27:59'),
+(10, 5, '10000.00', '2020-10-14 10:31:13'),
+(11, 6, '1000.00', '2021-04-01 13:37:59'),
+(12, 6, '0.00', '2021-04-01 13:38:04'),
+(13, 7, '4000.00', '2021-04-01 13:44:51'),
+(14, 8, '20000.00', '2021-04-01 13:45:58'),
+(15, 9, '35000.00', '2021-04-01 13:47:44'),
+(16, 10, '10000.00', '2021-04-01 13:48:52');
 
-DROP TABLE IF EXISTS `bet`;
+-- --------------------------------------------------------
 
-CREATE TABLE `bet` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ticket_id` int(10) unsigned NOT NULL,
-  `amount` decimal(14,2) NOT NULL,
-  `bet_type_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `ticket_id` (`ticket_id`),
-  KEY `bet_type_id` (`bet_type_id`),
-  CONSTRAINT `bet_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`id`),
-  CONSTRAINT `bet_ibfk_2` FOREIGN KEY (`bet_type_id`) REFERENCES `bet_type` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Table structure for table `user`
+--
 
-/*Data for the table `bet` */
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `bet_type` */
+--
+-- Dumping data for table `user`
+--
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO `user` (`id`, `username`, `password`, `created`) VALUES
+(1, 'kenoman', '5hfefomvncdva', '2021-03-31 18:25:08'),
+(2, 'luckyhope', 'lvbckyh3jpjfgfb', '2021-03-31 18:26:17'),
+(3, 'rouletter', 'r6hfdulftf5ter$f', '2021-03-31 18:28:56'),
+(4, 'jblooker', 'hafg5ypygahfd@er', '2021-03-31 18:30:57'),
+(5, 'zafko', 'tf46&epla2$e$', '2021-03-31 18:32:32'),
+(6, 'dreamcatcher', 'dr0934rch45!65', '2021-03-31 18:33:33'),
+(7, 'fortunateag', 'f0cvlmawrInjd:Lc', '2021-03-31 18:37:57'),
+(8, 'roulettist', 'f4klfasdfk9jU*HuJ', '2021-03-31 18:38:34'),
+(9, 'vintagecasino', 'dvfsb*&HBJVYUTDFp0', '2021-03-31 18:40:23'),
+(10, 'smokygambler', 'dvkL?v90wr0kdcx', '2021-03-31 18:41:19');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bet`
+--
+ALTER TABLE `bet`
+  ADD CONSTRAINT `bet_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`id`),
+  ADD CONSTRAINT `bet_ibfk_2` FOREIGN KEY (`bet_type_id`) REFERENCES `bet_type` (`id`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
